97a76afbbf1e253ad5f34020dfa4f7aadfb7881d
Daniel L. Polanco <dlpolanco05@gmail.com>
2016-01-10 19:24:27 -0700

    Add icon information for views

--- src/main/resources/static/js/routes.js
+++ src/main/resources/static/js/routes.js
@@ -27,18 +27,26 @@ angular.module('Mccy.routes', [
         {
             view: '/view',
             label: 'Current Containers'
+	    icon: 'icon fa fa-heartbeat'
+	    show: true
         },
         {
             view: '/new-server',
             label: 'New Container'
+	    icon: 'icon fa fa-magic'
+	    show: true
         },
         {
             view: '/upload-mod',
             label: 'Upload Mods'
+	    icon: 'icon fa fa-upload'
+	    show: true
         },
         {
             view: '/manage-mods',
             label: 'Manage Mods'
+	    icon: 'icon fa fa-flask'
+	    show: true
         }
     ])
 ;
