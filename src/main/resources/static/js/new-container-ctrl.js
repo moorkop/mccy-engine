d2de5106504300fe6cac6bdf91eb52e1455fad10
Geoff Bourne <itzgeoff@gmail.com>
2016-03-06 09:16:49 -0600

    Version list wasn't getting filled when creating a new container

--- src/main/resources/static/js/new-container-ctrl.js
+++ src/main/resources/static/js/new-container-ctrl.js
@@ -20,7 +20,7 @@ angular.module('Mccy.NewContainerCtrl', [
         });
 
         Versions.query({type:'VANILLA'}, function(response){
-            $scope.versions = $scope.versions.concat(response.map(function(v){
+            $scope.applicableVersions = $scope.versions.concat(response.map(function(v){
                 return {
                     value: v,
                     label: v
